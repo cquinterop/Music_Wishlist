b46057d490278419fca91dacf65caee388ed81c6
cquinterop <cristianqpineda@gmail.com>
2019-11-30 11:09:20 -0500

    Updated dependencies

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
 const config = require('../../client/src/config/config')
 
-mongoose.connect(config.DB, { useNewUrlParser: true, useFindAndModify: false })
+mongoose.connect(config.DB, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+})
     .then(res => console.log('Successfully connected to database.'))
     .catch(err => console.log('DB conection error', err))
 
